ecf7c59a9f44564fd236afebcd8b2d966df3db94
Axel Jeunet--Bailly <axel.jeunet--bailly@grenoble-inp.org>
2025-01-15 21:47:56 +0100

    (fix) Remove c++

--- backend/include/parser/CsvMarketDataParser.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "MarketDataParser.hpp"
-
-class CsvMarketDataParser : public MarketDataParser {
-    public:
-        std::vector<MarketData> parse(const std::string& filename) override;
-};
